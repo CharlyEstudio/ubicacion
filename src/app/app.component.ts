909e96c98587505d62642fa22c0b929984d6ceb6
CharlyEstudio <oingestudio@gmail.com>
2019-07-11 17:41:27 -0500

    Proyecto Ubicación de Productos en el Almacén

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ubicacion';
+
+  @ViewChild('inputCodigo') inputCodigo: ElementRef;
+  producto: any;
+  encontrado = false;
+
+  msg = '';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  obtenerUbicacion() {
+    this.msg = '';
+    this.encontrado = false;
+    if (this.inputCodigo.nativeElement.value === '') {
+      this.msg = 'Debe de ingresar un código.';
+      return;
+    }
+    const url = 'https://ferremayoristas.com.mx:3001/almacen/ubicacion/' + this.inputCodigo.nativeElement.value;
+    this.http.get(url).subscribe((ubicado: any) => {
+      if (ubicado.status) {
+        this.encontrado = true;
+        this.producto = ubicado.respuesta[0];
+      }
+    });
+  }
+
 }
